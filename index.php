877723c68cea48277c678f3928ca39c7a5e85c66
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-02-02 00:41:50 +0100

    :sparkles: index.php: redirects now when tokens are not set, renders orders list.

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 require __DIR__ . '/vendor/autoload.php';
 
 include("auth.php");
+include("app.php");
 include("helpers.php");
 
-use \ready2order\Client;
+use ready2order\Client;
 
 /* Used to load private key from .env file */
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
@@ -14,17 +15,23 @@ $dotenv->safeLoad();
 session_start();
 
 $URL = $_SERVER['REQUEST_URI'];
+$SERVER_ADDRESS = "http://{$_SERVER['HTTP_HOST']}";
 
-if (parse_url($URL, PHP_URL_PATH) === '/auth') {
-    $grantAccessResponse = auth_as_developer($_ENV['DEVELOPER_TOKEN'], 'http://' . $_SERVER['HTTP_HOST'] . '/granted');
+$PARSED_URL = parse_url($URL, PHP_URL_PATH);
+
+if ($PARSED_URL === '/auth' || ($PARSED_URL !== '/granted' && /* check if tokens are set */ (!isset($_SESSION['grantAccessToken']) || !isset($_SESSION['accountToken'])))) {
+    $grantAccessResponse = auth_as_developer($_ENV['DEVELOPER_TOKEN'], $SERVER_ADDRESS . '/granted');
     $_SESSION['grantAccessToken'] = $grantAccessResponse['grantAccessToken'];
+
     /* Redirect to ready2order authorization page */
     header('Location: ' . $grantAccessResponse['grantAccessUri'], true, 301);
-} else if (parse_url($URL, PHP_URL_PATH) === '/granted') {
+} else if ($PARSED_URL === '/token') {
+    echo "<div>DEVELOPER TOKEN: {$_ENV['DEVELOPER_TOKEN']}</div><div>GRANT ACCESS TOKEN: {$_SESSION['grantAccessToken']}</div>";
+} else if ($PARSED_URL === '/granted') {
     /* Get status and grantAccessToken from ready2order */
     $status = get_value('status');
     $grantAccessToken = get_value('grantAccessToken');
-    if ($status !== 'approved' /* check if status */ || $grantAccessToken !== $_SESSION['grantAccessToken'] /* Check if grantAccessToken is valid */) {
+    if ($status !== 'approved' /* check status */ || $grantAccessToken !== $_SESSION['grantAccessToken'] /* Check if grantAccessToken is valid */) {
         echo 'ERROR! Not Authorized';
         exit;
     }
@@ -34,8 +41,8 @@ if (parse_url($URL, PHP_URL_PATH) === '/auth') {
     $_SESSION['accountToken'] = $accountToken;
 
     /* Redirect to index.php */
-    header('Location: ' . 'http://' . $_SERVER['HTTP_HOST'], true, 301);
+    header('Location: ' . $SERVER_ADDRESS, true, 301);
 } else {
     $client = new Client($_SESSION['accountToken']);
-    print_r($client->get('orders'));
+    print_all_orders(get_all_orders($client));
 }
