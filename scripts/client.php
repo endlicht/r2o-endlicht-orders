4749e287cf98cc47015c7b364551dc80ca67f186
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-02-07 00:11:10 +0100

    client.php: better logic.
    Signed-off-by: jpkmiller <46693545+jpkmiller@users.noreply.github.com>

--- scripts/client.php
+++ scripts/client.php
@@ -5,7 +5,6 @@
 *
 *    Please see LICENSE file for your rights under this license. */
 
-use JetBrains\PhpStorm\Pure;
 use ready2order\Client;
 
 /**
@@ -13,19 +12,25 @@ use ready2order\Client;
  * @param string|null $accountToken
  * @return false|Client
  */
-#[Pure] function get_client(string|null $accountToken = null): false|Client
+function get_client(string|null $accountToken = null): false|Client
 {
+    $client = false;
     if (isset($_SESSION['client'])) {
         return $_SESSION['client'];
     }
     if ($accountToken !== null) {
         /* Try accountToken from param to create new Client */
-        return new Client($accountToken);
+        $client = new Client($accountToken);
     }
     if (isset($_SESSION['accountToken'])) {
         /* Try accountToken from session to create new Client */
-        return new Client($_SESSION['accountToken']);
+        $client = new Client($_SESSION['accountToken']);
     }
-    /* If accountToken is not set in session return */
-    return false;
+
+    if ($client !== false) {
+        /* Save Client to session */
+        $_SESSION['client'] = $client;
+    }
+
+    return $client;
 }
