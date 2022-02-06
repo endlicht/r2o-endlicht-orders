55cebc96f30765ab0fff4c281a15e539d81fd3f1
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-02-06 21:28:53 +0100

    added dailyReport.php: shows whether the day is opened.
    Signed-off-by: jpkmiller <46693545+jpkmiller@users.noreply.github.com>

---
+++ template/dailyReport.php
@@ -0,0 +1,32 @@
+<?php
+/*
+*    endlicht-r2o-back: The endlicht ready to order application.
+*    Copyright (c) 2022 Josef Müller
+*
+*    Please see LICENSE file for your rights under this license. */
+
+$client = get_client();
+if ($client === false) {
+    return;
+}
+
+try {
+    $status = $client->get('dailyReport/status');
+} catch (Exception $e) {
+    return;
+}
+
+$open = $status['status'] === 'open';
+$_SESSION['open'] = $open;
+
+if ($open) {
+    ?>
+    <h3>Hallo. Hier siehst du die letzten Bestellungen.</h3>
+    <?php
+} else {
+    ?>
+    <h3>Der Tag ist geschlossen. Bis morgen :)</h3>
+    <?php
+}
+?>
+
