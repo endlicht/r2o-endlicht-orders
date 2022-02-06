ca91888f2ecffafabd8104bd4396efdd5b51a169
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-02-06 21:27:50 +0100

    helpers.php: updated comments and added copyright.
    Signed-off-by: jpkmiller <46693545+jpkmiller@users.noreply.github.com>

--- scripts/helpers.php
+++ scripts/helpers.php
@@ -1,9 +1,14 @@
 <?php
+/*
+*    endlicht-r2o-back: The endlicht ready to order application.
+*    Copyright (c) 2022 Josef Müller
+*
+*    Please see LICENSE file for your rights under this license. */
 
 use JetBrains\PhpStorm\Pure;
 
 /**
- * Escape all html characters.
+ * Escape all HTML characters.
  * @param string|null $string $string
  * @return string
  */
@@ -17,10 +22,10 @@ function clean_string(string|null $string): string
 
 /**
  * Get value from global $_GET variable.
- * @param string $key
+ * @param string|null $key
  * @return string
  */
-#[Pure] function get_value(string $key): string
+#[Pure] function get_value(string|null $key): string
 {
     if ($key !== null && isset($_GET[$key])) {
         return clean_string($_GET[$key]);
