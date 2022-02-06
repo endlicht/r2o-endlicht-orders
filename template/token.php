7811aecab02d2752d2f32ac6a3c37f9b3121639c
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-02-06 21:24:46 +0100

    added token.php: displays all tokens.

---
+++ template/token.php
@@ -0,0 +1,5 @@
+<ul>
+    <li>DEVELOPER TOKEN: <code><?php echo $_ENV['DEVELOPER_TOKEN'] ?></code></li>
+    <li>GRANT ACCESS TOKEN: <code><?php echo $_SESSION['grantAccessToken'] ?></code></li>
+    <li>ACCOUNT TOKEN: <code><?php echo $_SESSION['accountToken'] ?></code></li>
+</ul>
