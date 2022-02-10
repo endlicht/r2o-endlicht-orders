1c16dda68666ea347750b74d9dd02e388bd79c0f
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-02-11 00:31:24 +0100

    :sparkles: header.php: added separate header template.
    Signed-off-by: jpkmiller <46693545+jpkmiller@users.noreply.github.com>

---
+++ template/header.php
@@ -0,0 +1,20 @@
+<?php
+
+$TITLE = get_company_name() . " Bestellungen";
+?>
+
+<html lang="de">
+<head>
+    <meta charset="utf-8">
+    <meta content="IE=edge" http-equiv="X-UA-Compatible">
+    <meta content="default-src 'none'; base-uri 'none'; child-src 'self'; form-action 'self'; frame-src 'self'; font-src 'self'; connect-src 'self'; img-src 'self'; manifest-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'"
+          http-equiv="Content-Security-Policy">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Bestellungen">
+    <meta name="author" content="Josef Müller">
+
+    <!-- Boostrap CSS -->
+    <link rel="stylesheet" href="../style/vendor/bootstrap.min.css">
+
+    <title><?php echo $TITLE; ?></title>
+</head>
