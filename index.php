fdf7ecbf009ac6438d0407c78f0de34af658a0b0
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-02-12 22:30:35 +0100

    index.php: cleaned up due to arch changes.
    Signed-off-by: jpkmiller <46693545+jpkmiller@users.noreply.github.com>

--- index.php
+++ index.php
@@ -9,8 +9,6 @@ require __DIR__ . '/vendor/autoload.php';
 
 include("scripts/r2o-orders/php/auth.php");
 include("scripts/r2o-orders/php/helpers.php");
-include_once("scripts/r2o-orders/php/client.php");
-include_once("scripts/r2o-orders/php/checkAuth.php");
 include_once("scripts/r2o-orders/php/company.php");
 
 /* Used to load private key from .env file */
@@ -21,49 +19,26 @@ session_start();
 
 $URL = $_SERVER['REQUEST_URI'];
 $SERVER_ADDRESS = "http://{$_SERVER['HTTP_HOST']}";
+$_SESSION['SERVER_ADDRESS'] = $SERVER_ADDRESS;
 
 $PARSED_URL = parse_url($URL, PHP_URL_PATH);
-
-$TITLE = get_company_name() . " Bestellungen";
 
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
+
+/* Read account token from cache if it exists */
+update_and_get_account_token();
 
 ?>
 <!doctype html>
 <html lang="de">
 <?php require("template/header.php"); ?>
 <body>
-<nav class="navbar navbar-dark bg-dark mb-3">
-    <div class="container-fluid">
-        <h1 class="navbar-brand"><?php echo $TITLE; ?></h1>
-        <ul class="navbar-nav m-auto">
-            <?php if (!is_logged_in()) { ?>
-                <li class="nav-item fs-4">
-                    <!-- Link to authenticate at ready2order API -->
-                    <a href="<?php echo $SERVER_ADDRESS . '/auth'; ?>" class="nav-link">Anmelden</a>
-                </li>
-            <?php } ?>
-            <?php if (is_logged_in()) { ?>
-                <li class="nav-item fs-4">
-                    <!-- Link to refresh page -->
-                    <a href="<?php echo $SERVER_ADDRESS; ?>" class="nav-link">Aktualisieren</a>
-                </li>
-            <?php } ?>
-        </ul>
-        <span class="navbar-text text-secondary">
-            <!-- Show last time the page was refreshed -->
-            <?php echo "Letzte Aktualisierung: " . date("d.m.Y H:i:s"); ?>
-        </span>
-    </div>
-</nav>
-
+<?php require("template/navigation.php"); ?>
 <div class="container-fluid my-3">
 
     <?php
-
-    $client = get_client();
 
     if ($PARSED_URL === '/auth') {
         /* Authenticate developer at ready2order API */
@@ -85,22 +60,25 @@ header("Pragma: no-cache");
         /* Get status and grantAccessToken from ready2order */
         $status = get_value('status');
         $grantAccessToken = get_value('grantAccessToken');
-        if ($status !== 'approved' /* check status */ || $grantAccessToken !== $_SESSION['grantAccessToken'] /* Check if grantAccessToken is valid */) {
+        if ($status !== 'approved' /* check status */ || !isset($_SESSION['grantAccessToken']) || $grantAccessToken !== $_SESSION['grantAccessToken'] /* Check if grantAccessToken is valid */) {
             ?><h2>Ein Fehler ist beim Anmelden bei ready2order aufgetreten!</h2><?php
             exit();
         }
 
-        /* Get accountToken from ready2order and save it as a SESSION Token */
-        $accountToken = get_value('accountToken');
 
         /* Restart session and invalidate session data */
         session_destroy();
         session_start();
 
         /* Save tokens and client to session */
-        $_SESSION['accountToken'] = $accountToken;
         $_SESSION['grantAccessToken'] = $grantAccessToken;
-        $_SESSION['client'] = get_client($accountToken);
+
+        /* Get accountToken from ready2order and save it as a SESSION Token */
+        $accountToken = get_value('accountToken');
+        update_and_get_account_token($accountToken);
+
+        /* Update client */
+        get_client_if_logged_in();
 
         /* Redirect to index.php */
         header('Location: ' . $SERVER_ADDRESS, true, 302);
