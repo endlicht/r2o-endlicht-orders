5cd5a5e6f3d8355a8e5ef21ef50bc5a8abe051d9
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-02-13 21:51:23 +0100

    navbar.php: added code to toggle sidebar.
    Signed-off-by: jpkmiller <46693545+jpkmiller@users.noreply.github.com>

--- template/navbar.php
+++ template/navbar.php
@@ -6,7 +6,9 @@ $SERVER_ADDRESS = create_internal_link();
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
     <ul class="navbar-nav">
         <li class="nav-item">
-            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+            <a class="nav-link" id="sidebar-toggler" href="#" role="button">
+                <i class="fas fa-bars"></i>
+            </a>
         </li>
         <?php if (!$is_logged_in) { ?>
             <li class="nav-item d-none d-sm-inline-block">
@@ -34,3 +36,48 @@ $SERVER_ADDRESS = create_internal_link();
         </li>
     </ul>
 </nav>
+<script>
+    /* Toggle between showing and hiding the sidebar */
+    document.addEventListener('DOMContentLoaded', () => {
+        /* When DOM is loaded add event listeners */
+        const TOGGLER = document.getElementById("sidebar-toggler")
+        const BODY = document.getElementsByTagName("BODY")[0]
+        const CONTENT = document.getElementsByClassName("content-wrapper")[0]
+
+        if (TOGGLER !== null) {
+            TOGGLER.addEventListener("click", () => {
+                if (window.screen.width >= 768) {
+                    if (!BODY.classList.contains("sidebar-collapse")) {
+                        BODY.classList.add("sidebar-collapse")
+                    } else {
+                        BODY.classList.remove("sidebar-collapse")
+                    }
+                    BODY.classList.remove("sidebar-open")
+                    BODY.classList.remove("sidebar-closed")
+                } else {
+                    BODY.classList.remove("sidebar-collapse")
+                    if (BODY.classList.contains("sidebar-open")) {
+                        BODY.classList.remove("sidebar-open")
+                        BODY.classList.add("sidebar-closed")
+                    } else {
+                        BODY.classList.add("sidebar-open")
+                        BODY.classList.remove("sidebar-closed")
+                    }
+                }
+            })
+        }
+
+        if (CONTENT !== null) {
+            CONTENT.addEventListener("click", () => {
+                if (768 <= window.screen.width && window.screen.width <= 992 && (BODY.classList.contains("sidebar-open") || !BODY.classList.contains("sidebar-collapse"))) {
+                    BODY.classList.remove("sidebar-open")
+                    BODY.classList.add("sidebar-collapse")
+                } else if (768 >= window.screen.width) {
+                    BODY.classList.remove("sidebar-collapse")
+                    BODY.classList.remove("sidebar-open")
+                    BODY.classList.add("sidebar-closed")
+                }
+            })
+        }
+    })
+</script>
