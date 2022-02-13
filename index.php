6898b561d2ccd1598f1e5d8d3e0d84082b543e1c
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-02-13 21:52:08 +0100

    :boom: index.php: moved most of the code to pages. - removed index_1.php :fire:
    Signed-off-by: jpkmiller <46693545+jpkmiller@users.noreply.github.com>

--- index.php
+++ index.php
@@ -7,14 +7,15 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-include("scripts/r2o-orders/php/auth.php");
-include("scripts/r2o-orders/php/helpers.php");
+include_once("scripts/r2o-orders/php/auth.php");
+include_once("scripts/r2o-orders/php/helpers.php");
 include_once("scripts/r2o-orders/php/company.php");
 
 /* Used to load private key from .env file */
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
+/* If session is not set start it */
 session_start();
 
 $URL = $_SERVER['REQUEST_URI'];
@@ -32,7 +33,7 @@ update_and_get_account_token();
 <!doctype html>
 <html lang="de">
 <?php include_once("template/head.php"); ?>
-<body class="hold-transition sidebar-mini">
+<body class="sidebar-mini sidebar-is-opening">
 <div class="wrapper">
 
     <?php
@@ -40,90 +41,22 @@ update_and_get_account_token();
     include_once("template/navbar.php");
     ?>
     <div class="content-wrapper">
-        <div class="content-header">
-            <div class="content-fluid">
-                <div class="col-sm-6">
-                    <h1 class="m-0">Dashboard</h1>
-                </div>
-            </div>
-        </div>
-        <div class="content">
-            <div class="content-fluid">
+        <?php
 
-
-                <?php
-
-                if ($PARSED_URL === '/auth') {
-                    /* Authenticate developer at ready2order API */
-                    try {
-                        $grantAccessResponse = auth_as_developer($_ENV['DEVELOPER_TOKEN'], create_internal_link('/granted'));
-                    } catch (JsonException $e) {
-                        ?><h2>Ein Fehler beim Authentifizieren als Entwickler ist aufgetreten!</h2><?php
-                        exit();
-                    }
-                    /* Safe grantAccessToken to session */
-                    $_SESSION['grantAccessToken'] = $grantAccessResponse['grantAccessToken'];
-
-                    /* Redirect to ready2order authorization page */
-                    header('Location: ' . $grantAccessResponse['grantAccessUri'], true, 302);
-                } else if ($PARSED_URL === '/token') {
-                    /* Show all tokens */
-                    require('template/token.php');
-                } else if ($PARSED_URL === '/logout') {
-                    /* Revoke access from ready2order */
-                    $client = get_client_if_logged_in();
-                    if ($client !== false) {
-                        $client->post('access/revoke');
-                        /* Remove session */
-                        session_destroy();
-                    }
-                } else if ($PARSED_URL === '/granted') {
-                    /* Get status and grantAccessToken from ready2order */
-                    $status = get_value('status');
-                    $grantAccessToken = get_value('grantAccessToken');
-                    if ($status !== 'approved' /* check status */ || !isset($_SESSION['grantAccessToken']) || $grantAccessToken !== $_SESSION['grantAccessToken'] /* Check if grantAccessToken is valid */) {
-                        ?><h2>Ein Fehler ist beim Anmelden bei ready2order aufgetreten!</h2><?php
-                        exit();
-                    }
-
-
-                    /* Restart session and invalidate session data */
-                    session_destroy();
-                    session_start();
-
-                    /* Save tokens and client to session */
-                    $_SESSION['grantAccessToken'] = $grantAccessToken;
-
-                    /* Get accountToken from ready2order and save it as a SESSION Token */
-                    $accountToken = get_value('accountToken');
-                    update_and_get_account_token($accountToken);
-
-                    /* Update client */
-                    get_client_if_logged_in();
-
-                    /* Redirect to index.php */
-                    header('Location: ' . create_internal_link(), true, 302);
-                } else {
-                    ?>
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <?php
-                            /* Show orders */
-                            include("template/orders.php");
-                            ?>
-                        </div>
-                        <div class="col-lg-6">
-                            <?php
-                            /* Inform if day is opened */
-                            include("template/report/report.php");
-                            ?>
-                        </div>
-                    </div>
-                    <?php
-                }
-                ?>
-            </div>
-        </div>
+        if ($PARSED_URL === '/auth') {
+            require("pages/auth.php");
+        } else if ($PARSED_URL === '/token') {
+            include_once('pages/token.php');
+        } else if ($PARSED_URL === '/logout') {
+            require("pages/logout.php");
+        } else if ($PARSED_URL === '/granted') {
+            require("pages/granted.php");
+        } else if ($PARSED_URL === '/') {
+            require("pages/dashboard.php");
+        } else {
+            include_once("pages/404.php");
+        }
+        ?>
     </div>
 </div>
 </body>
