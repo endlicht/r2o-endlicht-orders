af47b46b4edd1c3fc60bcc285da020130cf83c52
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-02-13 21:48:24 +0100

    500.php: added 500 page.
    Signed-off-by: jpkmiller <46693545+jpkmiller@users.noreply.github.com>

---
+++ pages/500.php
@@ -0,0 +1,21 @@
+<div class="content-header">
+    <div class="content-fluid">
+        <div class="col-sm-6">
+            <h1 class="m-0">500 Fehler</h1>
+        </div>
+    </div>
+</div>
+<div class="content">
+    <div class="error-page">
+        <h2 class="headline text-danger">500</h2>
+
+        <div class="error-content">
+            <h3><i class="fas fa-exclamation-triangle text-danger"></i> Uups! Etwas ist schief gelaufen.</h3>
+
+            <p>
+                Das Problem muss vom Entwickler behoben werden.
+                <a href="<?php echo create_internal_link() ?>">Zurück zur Startseite</a>
+            </p>
+        </div>
+    </div>
+</div>
