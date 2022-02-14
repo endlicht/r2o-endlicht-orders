785233ad62d3eeac0d6f4af0171857d82d2673d2
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-02-14 12:53:06 +0100

    logout.php: better logout information. Shows warning when logging out failed.
    Signed-off-by: jpkmiller <46693545+jpkmiller@users.noreply.github.com>

--- pages/logout.php
+++ pages/logout.php
@@ -1,11 +1,3 @@
-<?php
-/* Revoke access from ready2order */
-$client = get_client_if_logged_in();
-if ($client !== false) {
-    $client->post('access/revoke');
-}
-
-?>
 <div class="content-header">
     <div class="content-fluid">
         <div class="col-sm-6">
@@ -13,13 +5,33 @@ if ($client !== false) {
         </div>
     </div>
 </div>
-<div class="content">
-    <div class="container-fluid">
-        Weiterleitung zur Startseite...
+<?php
+/* Revoke access from ready2order */
+$client = get_client_if_logged_in();
+if ($client !== false) {
+    $client->post('access/revoke');
+    ?>
+    <div class="content">
+        <div class="container-fluid">
+            Weiterleitung zur Startseite...
+        </div>
     </div>
-</div>
-<script>
-    setTimeout(() => {
-        window.location.href = '<?php echo create_internal_link(); ?>';
-    }, 2000);
-</script>
+    <script>
+        setTimeout(() => {
+            window.location.href = '<?php echo create_internal_link(); ?>';
+        }, 2000);
+    </script>
+    <?php
+} else {
+    ?>
+    <div class="content">
+        <div class="container-fluid">
+            <h5 class="text-warning">
+                Abmeldung fehlgeschlagen.
+            </h5>
+        </div>
+    </div>
+    <?php
+}
+
+?>
