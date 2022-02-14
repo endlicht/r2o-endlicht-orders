d87b7c36009c175527c4f6ccde889fef63a1e44d
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-02-14 12:51:31 +0100

    dashboard.php: removed login and added licence information.
    Signed-off-by: jpkmiller <46693545+jpkmiller@users.noreply.github.com>

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1,47 +1,33 @@
 <?php
-$client = get_client_if_logged_in();
-if ($client === false) {
-    ?>
-    <div class="content-header">
-        <div class="content-fluid">
-            <div class="col-sm-6">
-                <h1 class="m-0">Hallo.</h1>
+/*
+*    r2o-orders: The simple way to show orders from r2o API.
+*    Copyright (c) 2022 Josef Müller
+*
+*    Please see LICENSE file for your rights under this license. */
+
+?>
+<div class="content-header">
+    <div class="content-fluid">
+        <div class="col-sm-6">
+            <h1 class="m-0">Dashboard</h1>
+        </div>
+    </div>
+</div>
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-6">
+                <?php
+                /* Show orders */
+                include("template/orders.php");
+                ?>
+            </div>
+            <div class="col-lg-6">
+                <?php
+                /* Inform if day is opened */
+                include("template/report/report.php");
+                ?>
             </div>
         </div>
     </div>
-    <div class="content">
-        <div class="container-fluid">
-            Bitte melde dich an.<br>
-            <a href="<?php echo create_internal_link('/auth'); ?>" class="link-primary">Anmelden</a>
-        </div>
-    </div>
-    <?php
-} else {
-    ?>
-    <div class="content-header">
-        <div class="content-fluid">
-            <div class="col-sm-6">
-                <h1 class="m-0">Dashboard</h1>
-            </div>
-        </div>
-    </div>
-    <div class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-6">
-                    <?php
-                    /* Show orders */
-                    include("template/orders.php");
-                    ?>
-                </div>
-                <div class="col-lg-6">
-                    <?php
-                    /* Inform if day is opened */
-                    include("template/report/report.php");
-                    ?>
-                </div>
-            </div>
-        </div>
-    </div>
-    <?php
-}
+</div>
