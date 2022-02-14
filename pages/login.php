8c039e5153738c1121568d905aadb3c585475267
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-02-14 12:52:26 +0100

    login.php: added login button.
    Signed-off-by: jpkmiller <46693545+jpkmiller@users.noreply.github.com>

--- pages/login.php
+++ pages/login.php
@@ -1,7 +1,21 @@
+<?php
+/*
+*    r2o-orders: The simple way to show orders from r2o API.
+*    Copyright (c) 2022 Josef Müller
+*
+*    Please see LICENSE file for your rights under this license. */
+
+?>
 <div class="content-header">
     <div class="content-fluid">
         <div class="col-sm-6">
             <h1 class="m-0">Hallo</h1>
         </div>
+    </div>
+</div>
+<div class="content">
+    <div class="container-fluid">
+        Bitte melde dich an.<br>
+        <a href="<?php echo create_internal_link('/auth'); ?>" class="link-primary">Anmelden</a>
     </div>
 </div>
