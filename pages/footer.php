d58502652cc28ad9db422af128019dfe3df9e327
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-02-14 12:51:41 +0100

    added footer.php: shows copyright.
    Signed-off-by: jpkmiller <46693545+jpkmiller@users.noreply.github.com>

---
+++ pages/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <strong>
+        Copyright &copy; 2022 <a href="https://github.com/jpkmiller">Josef Müller</a>.
+    </strong>
+    Alle Rechte vorbehalten.
+</footer>
