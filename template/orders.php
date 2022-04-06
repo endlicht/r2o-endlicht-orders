ccf1e7e08cf91892ec6cc5b12cc5e2275f8355bd
jpkmiller <46693545+jpkmiller@users.noreply.github.com>
2022-04-06 15:10:42 +0200

    orders.php: can now be clicked :)
    Signed-off-by: jpkmiller <46693545+jpkmiller@users.noreply.github.com>

--- template/orders.php
+++ template/orders.php
@@ -11,17 +11,22 @@ if ($client === false) {
 }
 
 /* Define keys for table */
-$key_name = 'item_product_name';
+$key_name = 'item_name';
 $key_quantity = 'item_quantity';
 $key_timestamp = 'item_timestamp';
 $key_timestamp_invoice = 'invoice_timestamp';
+$key_item_id = 'item_id';
 
 $PAST_HOURS = 24;
 /* Current date */
 $timestamp_now = date('Y-m-d', time() - (60 * 60 * $PAST_HOURS));
 
-function get_orders(string|null $date = null): array|false
+function get_orders(string|null $date = null, string|null $test_data = null): array|false
 {
+    if ($test_data !== null) {
+        return $test_data;
+    }
+
     global $client, $timestamp_now;
     if ($date === null) {
         $date = $timestamp_now;
@@ -42,6 +47,18 @@ if ($orders === false) {
 $amount_orders = 0;
 
 ?>
+<style>
+    .order-item {
+        cursor: pointer;
+        padding-top: 5px;
+        padding-bottom: 5px;
+        font-size: larger;
+    }
+
+    .order-item.finished {
+        background-color: lightgreen !important;
+    }
+</style>
 <div class="card">
     <div class="card-header border-0">
         <div class="d-flex justify-content-between">
@@ -73,7 +90,8 @@ $amount_orders = 0;
                         /* Time as string */
                         $time = date('H:i, d.m.Y', strtotime($item[$key_timestamp]));
                         ?>
-                        <tr>
+                        <tr onclick="toggle_item_as_finished(<?php echo $item[$key_item_id] ?>)"
+                            id="<?php echo 'item_' . $item[$key_item_id] ?>" class="order-item">
                             <td class="product-name"><?php echo $item[$key_name] ?></td>
                             <td class="product-quantity"><?php echo $item[$key_quantity] ?></td>
                             <td colspan="100%" class="timestamp"><?php echo $time ?></td>
@@ -81,7 +99,6 @@ $amount_orders = 0;
                     <?php }
                 }
             }
-
             $_SESSION['amount_orders'] = $amount_orders;
 
             ?>
@@ -89,3 +106,24 @@ $amount_orders = 0;
         </table>
     </div>
 </div>
+<script>
+    function toggle_item_as_finished(orderId) {
+        if (is_finished('item_' + orderId)) {
+            localStorage.setItem('item_' + orderId, '0');
+            document.getElementById('item_' + orderId).classList.remove('finished');
+        } else {
+            localStorage.setItem('item_' + orderId, '1');
+            document.getElementById('item_' + orderId).classList.add('finished');
+        }
+    }
+
+    function is_finished(orderId) {
+        return (localStorage.getItem(orderId) ?? '0') === '1';
+    }
+
+    Array.from(document.getElementsByClassName('order-item')).forEach((el) => {
+        if (is_finished(el.id)) {
+            el.classList.add('finished');
+        }
+    })
+</script>
